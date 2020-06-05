d54d92afe59d42ceffd879fa74bbbe415d1e9a0f
Joseph Flores <yosept.flores@gmail.com>
2020-06-05 10:52:51 -0500

    Minor liner error fixed

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,7 +1,7 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Button from './Button';
 import './ButtonPanel.scss';
-import PropTypes from 'prop-types';
 
 const ButtonPanel = ({ clickHandler }) => {
   const symbols = [
